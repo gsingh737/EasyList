4e68bf044cdaf06ee7ca3863658439a86627f6b3
Gurpreet Singh <gurpreetsingh@Gurpreets-iMac.local>
2019-03-07 21:43:29 -0500

    Cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,17 @@
 import 'package:flutter/material.dart';
 
+import './product_manager.dart';
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Food Tested'];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: Text('EasyList'),
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(10.0),
-            child: RaisedButton(
-              onPressed: () {
-                setState(() {
-                  _products.add('Advance Food Tester');                  
-                });
-              },
-              child: Text('Add product'),
-            ),
-          ),
-        ],
-      ),
+      body: ProductManager(),
     ));
   }
 }
