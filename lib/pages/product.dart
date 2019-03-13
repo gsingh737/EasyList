dc4b4aafc38dfe5a33e1ee04427a6008d8b76355
Gurpreet Singh <gurpreetsingh@Gurpreets-iMac.local>
2019-03-12 22:52:08 -0400

    update

---
+++ lib/pages/product.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ProductPage extends StatelessWidget {
+final String title;
+final String imageUrl;
+ProductPage(this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(imageUrl),
+          Container(
+            child: Text('Details!'),
+            padding: EdgeInsets.all(10.0),
+          ),
+          Container(
+            child: RaisedButton(
+              color: Theme.of(context).accentColor,
+              child: Text('BACK'),
+              onPressed: () => Navigator.pop(context, true),
+            ),
+            padding: EdgeInsets.all(10.0),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/products.dart
+++ lib/products.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import './pages/product.dart';
 
 class Products extends StatelessWidget {
-  final List<String> products;
+  final List<Map<String, String>> products;
 
   Products([this.products = const []]);
 
@@ -11,19 +11,21 @@ class Products extends StatelessWidget {
     return Card(
       child: Column(
         children: <Widget>[
-          Image.asset('assets/food.jpg'),
-          Text(products[index]),
+          Image.asset(products[index]['image']),
+          Text(products[index]['title']),
           ButtonBar(
             alignment: MainAxisAlignment.center,
             children: <Widget>[
               FlatButton(
                   child: Text('Details'),
-                  onPressed: () => Navigator.push(
+                  onPressed: () => Navigator.push<bool>(
                         context,
                         MaterialPageRoute(
-                          builder: (BuildContext context) => ProductPage(),
+                          builder: (BuildContext context) => ProductPage(
+                              products[index]['title'],
+                              products[index]['image']),
                         ),
-                      ))
+                      ).then((bool value) => print(value)))
             ],
           )
         ],
